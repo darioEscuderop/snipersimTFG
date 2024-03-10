e31af7a261b0f6b68e8b2695775fcb03ae21019b
Dario <dario.escuderop@um.es>
2024-03-10 23:24:09 +0100

    fix page_stats evicted

--- common/core/memory_subsystem/page_stats.h
+++ common/core/memory_subsystem/page_stats.h
@@ -8,7 +8,7 @@
 class PageStats {
 public:
     PageStats(bool ejecucion, core_id_t core_id);
-    void updatePageStats(int pageAdress, bool evict);
+    void updatePageStats(int pageAdress, bool evict = false);
 
 private:
     std::unordered_map<int, std::bitset<1024>> pageMap; // Asumimos que el tamano de la pagina es 4096 bytes (1024 palabras de 4 bytes)
